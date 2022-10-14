8871311e7a59230acda63e0458e15fd864742cdb
tieubach2k1 <tieubachzzz2k1@gmail.com>
2022-10-14 12:55:11 +0700

    Change project

---
+++ skype_clone/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:skype_clone/resources/firebase_repository.dart';
+import 'package:skype_clone/screens/home_screen.dart';
+import 'package:skype_clone/screens/login_screen.dart';
+import 'package:skype_clone/screens/search_screen.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  FirebaseRepository _repository = FirebaseRepository();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Skype Clone",
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/search_screen': (context) => SearchScreen(),
+      },
+      theme: ThemeData(
+          brightness: Brightness.dark
+      ),
+      home: FutureBuilder(
+        future: _repository.getCurrentUser(),
+        builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+          if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return LoginScreen();
+          }
+        },
+      ),
+    );
+  }
+}
